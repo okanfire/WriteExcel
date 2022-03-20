364a66f481833edc5026bf3f5301dcdaf79eba8f
okanfire <okan0083@gmail.com>
2022-03-20 13:07:45 +0900

    PSの微修正

--- WriteExcel.ps1
+++ WriteExcel.ps1
@@ -1,6 +1,6 @@
 function WriteExcel($Sheet){
     $PSpath = Get-Location | ForEach-Object Path
     
-    py -3 WriteExcel.py $PSpath $Sheet
-    #Start-Process -FilePath C:\Users\okanfire\Desktop\code\dist\WriteExcel.exe -ArgumentList $PSpath -Wait
+    #py -3 WriteExcel.py $PSpath $Sheet
+    Start-Process -FilePath C:\Users\okanfire\Desktop\code\WriteExcel\dist\WriteExcel.exe -ArgumentList $PSpath,$Sheet -Wait
 }
